b82e930e001f30ba8dd76648d209fe307c142854
mon33k <moniquemojica@ac.c4q.nyc>
2018-07-15 21:39:39 -0400

    need to do more of these

--- codewars/formatStringOfNames.js
+++ codewars/formatStringOfNames.js
@@ -16,3 +16,31 @@
 // // returns ''
 // Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
 
+const list = (arr) => {
+    if(arr.length === 1) {
+        return arr[0].name
+    }
+
+    if(arr.length === 2) {
+        return arr[0].name + "&" + arr[1].name
+    } 
+    let str = ''
+    for(var i = 0; i < arr.length; i++) {
+        if(i === arr.length -2){ // if the index refers to the second to last element then take whatever str is and that element and add a space 
+            str = str + arr[i].name + ' '
+        }
+        else if(i === arr.length - 1) { // if the index refers to the last element then take wutevr str is and add an & before the last element
+            str = str + "& " + arr[arr.length -1].name
+        }
+        else { // if i isnt the last or second to last element then take wutevr is already in str and for all other elems add a comma
+            str = str + arr[i].name + ", " 
+        }
+    }
+    return str
+}
+
+console.log(list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ]))
+
+const list2 = (arr) => {
+    
+}
