76a9724c29da2e03b478157a0c0bddb60d6ee089
mon33k <moniquemojica@ac.c4q.nyc>
2018-11-27 17:32:52 -0500

    added another one

--- CTCIGreenBook/ArraysStrings1_4.js
+++ CTCIGreenBook/ArraysStrings1_4.js
@@ -1,1 +1,17 @@
 // Palindrome Permutation:
+
+
+const isPalindrome = (str1, str2) => {
+    let reversed2ndStr = str2.split('').reverse().join('')
+    if(str1 === reversed2ndStr) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+
+isPalindrome('racecar', 'racecar')  // true
+isPalindrome('hello', 'whutis')     // false
+
+
+// ----- Do a diff solution with regex ----------- //
